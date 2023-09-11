dbca77a19ea12e65b709745aedb8d25ee14ab303
Camilla <104870685+CamillaHorneland@users.noreply.github.com>
2023-09-11 03:14:08 +0200

    working on allAuctions. Displaying right on page. Working on styling. Page back to realoding again

--- src/js/helpers/redirectBasedOnLogin.mjs
+++ src/js/helpers/redirectBasedOnLogin.mjs
@@ -1,15 +1,17 @@
-import { isLoggedIn } from "../storage/index.mjs";
+import { isLoggedIn } from "../storage/isLoggedIn.mjs";
 
 export function redirectBasedOnLogin(pathname) {
 	if (isLoggedIn()) {
 		console.log("Is logged in...");
 		if (pathname === "/profile/login/" || pathname === "/profile/register/index.html") {
-			//location.href = "/index.html";
 		}
 	} else {
 		console.log("Not logged in...");
 		if (pathname === "/index" || pathname === "/index.html") {
-			//location.href = "/index.html";
 		}
 	}
 }
+
+
+
+
